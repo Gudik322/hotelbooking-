04a69f9dd5bc72ad629ce92cfb2b7f6037c4dda3
Gudik <bryzhko.viktor98@gmail.com>
2025-05-21 19:40:45 +0300

    Update FD.jsx

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { roomsDummyData } from '../assets/assets'
+import HotelCard from './HotelCard'
 
 const FeaturedDestination = () => {
   return (
-    <div>FeaturedDestination</div>
+    <div>
+        <div>
+            {roomsDummyData.slice(0,4).map((room, index)=>(
+                <HotelCard key={room._id} room={room} index={index}/>
+            ))}
+        </div>
+    </div>
   )
 }
 
