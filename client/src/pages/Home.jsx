82829e2037e8f6675e6f1a1c9793cd40525fc04f
Gudik <bryzhko.viktor98@gmail.com>
2025-05-23 16:33:47 +0300

    Mod Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOfferse from '../components/ExclusiveOfferse'
 import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
        <FeaturedDestination />
        <ExclusiveOfferse />
        <Testimonial />
+       <NewsLetter />
     </>
   )
 }
