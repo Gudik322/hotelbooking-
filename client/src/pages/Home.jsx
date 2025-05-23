84715b5d722780a99034564d94df721a7a030777
Gudik <bryzhko.viktor98@gmail.com>
2025-05-23 15:38:37 +0300

    Add EO in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOfferse from '../components/ExclusiveOfferse'
 
 const Home = () => {
   return (
     <>
        <Hero />
        <FeaturedDestination />
+       <ExclusiveOfferse />
     </>
   )
 }
